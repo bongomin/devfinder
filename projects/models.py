f1785ef8feb4c553d0a90425382845e8030aed53
Bongomin Daniel <bongomindaniel@gmail.com>
2022-08-07 14:09:01 +0300

    install templates

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,8 @@ import uuid
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
-    demo_link = models.URLField(max_length=2000,null=True,blank=True)
-    source_link = models.URLField(max_length=2000,null=True,blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag',blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
