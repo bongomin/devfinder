9ac01eb48fb243bc9772ce2a1e24ed7b88c4ce7f
Bongomin Daniel <bongomindaniel@gmail.com>
2022-08-11 05:41:00 +0300

    impliment the Profiles Page , Routers, views and model

--- users/models.py
+++ users/models.py
@@ -8,15 +8,28 @@ class Profile(models.Model):
     email = models.EmailField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     bio =  models.TextField(max_length=200, null=True, blank=True)
     profile_img = models.ImageField(null=True, blank=True, upload_to='profiles/' ,default='profiles/user-default.png')
     social_github = models.CharField(max_length=200, null=True, blank=True)
     social_twitter = models.CharField(max_length=200, null=True, blank=True)
     social_linkedin = models.CharField(max_length=200, null=True, blank=True)
     social_youtube = models.CharField(max_length=200, null=True, blank=True)
+    social_website = models.CharField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
 
 
     def __str__(self):
         return str(self.user.username)
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+    
