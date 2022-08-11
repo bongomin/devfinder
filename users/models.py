5bd090ad778c63462f023e71e8be82bf4a435177
Bongomin Daniel <bongomindaniel@gmail.com>
2022-08-11 06:53:30 +0300

    impliment django signals

--- users/models.py
+++ users/models.py
@@ -19,9 +19,8 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
 
-
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
@@ -32,4 +31,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
-    
