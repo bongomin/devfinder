640ab096715621ea213ef4bde66dc43667d2d4a2
Bongomin Daniel <bongomindaniel@gmail.com>
2022-08-12 07:52:02 +0300

    add sign in and sign up templates

--- users/forms.py
+++ users/forms.py
@@ -10,3 +10,9 @@ class customUserCreationForm(UserCreationForm):
         labels = { 
             'first_name': 'Full name',
         }
+
+    def __init__(self, *args, **kwargs):
+        super(customUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
